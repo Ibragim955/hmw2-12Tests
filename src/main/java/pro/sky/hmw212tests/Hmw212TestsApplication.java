e910f750a931fdd7deaf143fded39ae5ba3de98a
ibra <amerhanov_ibragim95@mail.ru>
2023-07-11 00:59:53 +0300

    first'

---
+++ src/main/java/pro/sky/hmw212tests/Hmw212TestsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hmw212tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hmw212TestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hmw212TestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/hmw212tests/Hmw212TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hmw212tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hmw212TestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
